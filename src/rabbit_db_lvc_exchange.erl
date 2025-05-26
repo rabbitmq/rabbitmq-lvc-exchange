845878c285279d867afe129bd0db22706153a801
Lois Soto Lopez <lois@84codes.com>
2025-05-26 09:57:02 +0200

    Update licenses

--- src/rabbit_db_lvc_exchange.erl
+++ src/rabbit_db_lvc_exchange.erl
@@ -2,7 +2,8 @@
 %% License, v. 2.0. If a copy of the MPL was not distributed with this
 %% file, You can obtain one at https://mozilla.org/MPL/2.0/.
 %%
-%% Copyright (c) 2007-2012 VMware, Inc.  All rights reserved.
+%% Copyright (c) 2007-2025 Broadcom. All Rights Reserved. The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries. All rights reserved.
+%%
 -module(rabbit_db_lvc_exchange).
 
 -include_lib("rabbit_common/include/rabbit.hrl").
